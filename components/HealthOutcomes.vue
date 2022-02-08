24ce5cffde17224c427fec6dde9437eea90a6092
dino16m <anselem16m@gmail.com>
2022-02-08 08:14:26 +0100

    lint fix

--- components/HealthOutcomes.vue
+++ components/HealthOutcomes.vue
@@ -1,36 +1,36 @@
 <template>
-  <div>
-    <div class="hidden my-12 mx-6 xl:grid xl:grid-cols-4 xl:gap-2">
-      <div
-        v-for="(choice, index) in choices"
-        :key="index"
-        class="py-12 px-6 cursor-pointer choices c-shadow"
-      >
-        <img class="mb-7" :src="choice.image" alt="" />
-        <span class="sub-titles-1">{{ choice.title }}</span>
-        <p class="mt-6">{{ choice.text }}</p>
-      </div>
-    </div>
+	<div>
+		<div class="hidden my-12 mx-6 xl:grid xl:grid-cols-4 xl:gap-2">
+			<div
+				v-for="(choice, index) in choices"
+				:key="index"
+				class="py-12 px-6 cursor-pointer choices c-shadow"
+			>
+				<img class="mb-7" :src="choice.image" alt="" />
+				<span class="sub-titles-1">{{ choice.title }}</span>
+				<p class="mt-6">{{ choice.text }}</p>
+			</div>
+		</div>
 
-    <div class="xl:hidden block">
-      <div v-swiper="swiperOption" :loadtheme="false">
-        <div class="swiper-wrapper">
-          <div
-            v-for="(choice, index) in choices"
-            :key="index"
-            class="swiper-slide py-12 px-6 cursor-pointer choices c-shadow"
-          >
-            <div class="flex flex-col items-center justify-center">
-              <img class="mb-7" :src="choice.image" alt="" />
-              <span class="text-center sub-titles-1">{{ choice.title }}</span>
-              <p class="text-center mt-6">{{ choice.text }}</p>
-            </div>
-          </div>
-        </div>
-        <div slot="pagination" class="swiper-pagination"></div>
-      </div>
-    </div>
-  </div>
+		<div class="xl:hidden block">
+			<div v-swiper="swiperOption" :loadtheme="false">
+				<div class="swiper-wrapper">
+					<div
+						v-for="(choice, index) in choices"
+						:key="index"
+						class="swiper-slide py-12 px-6 cursor-pointer choices c-shadow"
+					>
+						<div class="flex flex-col items-center justify-center">
+							<img class="mb-7" :src="choice.image" alt="" />
+							<span class="text-center sub-titles-1">{{ choice.title }}</span>
+							<p class="text-center mt-6">{{ choice.text }}</p>
+						</div>
+					</div>
+				</div>
+				<div slot="pagination" class="swiper-pagination"></div>
+			</div>
+		</div>
+	</div>
 </template>
 
 <script lang="ts">

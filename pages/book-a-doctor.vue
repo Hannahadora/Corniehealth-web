ea9ae1a0a76fc1dcb179ffb7be021fcfe5d1d11e
Hannah Chiamaka <hannahadora97@gmail.com>
2022-02-11 12:35:03 +0100

    update

--- pages/book-a-doctor.vue
+++ pages/book-a-doctor.vue
@@ -1,31 +1,31 @@
 <template>
-  <div class="">
-    <div class="mx-auto w-3/5 my-16">
-      <div class="flex flex-col items-center justify-center text-center">
-        <img
-          src="/images/book-appointment/unsplash_gkZ-k3xf25w (1).png"
-          alt=""
-        />
-      </div>
+	<div class="">
+		<div class="mx-auto w-3/5 my-16">
+			<div class="flex flex-col items-center justify-center text-center">
+				<img
+					src="/images/book-appointment/unsplash_gkZ-k3xf25w (1).png"
+					alt=""
+				/>
+			</div>
 
-      <div class="block text-center my-4">
-        <h3 class="mt-4 c-indigo">Dr. Mike Obi | M</h3>
+			<div class="block text-center my-4">
+				<h3 class="mt-4 c-indigo">Dr. Mike Obi | M</h3>
 
-        <span class="text-grey-blue mt-2">Mikeobi@reddington.com</span><br />
+				<span class="text-grey-blue mt-2">Mikeobi@reddington.com</span><br />
 
-        <div class="mt-4 flex items-center justify-center">
-          <img
-            class="mr-2"
-            src="/images/book-appointment/appointment-select.png"
-            alt=""
-          />
-          24, November 2021 | 14:00
-        </div>
-      </div>
+				<div class="mt-4 flex items-center justify-center">
+					<img
+						class="mr-2"
+						src="/images/book-appointment/appointment-select.png"
+						alt=""
+					/>
+					24, November 2021 | 14:00
+				</div>
+			</div>
 
-      <nuxt-child></nuxt-child>
-    </div>
-  </div>
+			<nuxt-child></nuxt-child>
+		</div>
+	</div>
 </template>
 
 <script lang="ts">
--- pages/book-a-doctor/step2.vue
+++ pages/book-a-doctor/step2.vue
@@ -1,98 +1,98 @@
 <template>
-  <div class="">
-    <div>
-      <h4 class="mt-12 text-center">Have Insurance?</h4>
+	<div class="">
+		<div>
+			<h4 class="mt-12 text-center">Have Insurance?</h4>
 
-      <form action="" class="flex items-center justify-center mt-4">
-        <div class="mr-9">
-          <input
-            name="insurance"
-            type="radio"
-            value="yes"
-            v-model="haveInsurance"
-          />
-          <label for="">Yes</label>
-        </div>
-        <div>
-          <input
-            name="insurance"
-            type="radio"
-            value="no"
-            v-model="haveInsurance"
-          />
-          <label for="">No</label>
-        </div>
-      </form>
-    </div>
+			<form action="" class="flex items-center justify-center mt-4">
+				<div class="mr-9">
+					<input
+						v-model="haveInsurance"
+						name="insurance"
+						type="radio"
+						value="yes"
+					/>
+					<label for="">Yes</label>
+				</div>
+				<div>
+					<input
+						v-model="haveInsurance"
+						name="insurance"
+						type="radio"
+						value="no"
+					/>
+					<label for="">No</label>
+				</div>
+			</form>
+		</div>
 
-    <div class="mt-8" v-if="haveInsurance === 'yes'">
-      <h4 class="mt-12 text-center">
-        Is your insurance provided by an employer or private?
-      </h4>
+		<div v-if="haveInsurance === 'yes'" class="mt-8">
+			<h4 class="mt-12 text-center">
+				Is your insurance provided by an employer or private?
+			</h4>
 
-      <form action="" class="flex items-center justify-center mt-4">
-        <div class="mr-9">
-          <input
-            type="radio"
-            name="insuranceprovider"
-            value="employer"
-            v-model="insuranceprovider"
-          />
-          <label for="">Employer</label>
-        </div>
-        <div>
-          <input
-            name="insuranceprovider"
-            type="radio"
-            value="private"
-            v-model="insuranceprovider"
-          />
-          <label for="">Private</label>
-        </div>
-      </form>
-    </div>
+			<form action="" class="flex items-center justify-center mt-4">
+				<div class="mr-9">
+					<input
+						v-model="insuranceprovider"
+						type="radio"
+						name="insuranceprovider"
+						value="employer"
+					/>
+					<label for="">Employer</label>
+				</div>
+				<div>
+					<input
+						v-model="insuranceprovider"
+						name="insuranceprovider"
+						type="radio"
+						value="private"
+					/>
+					<label for="">Private</label>
+				</div>
+			</form>
+		</div>
 
-    <div v-if="insuranceprovider === 'employer'" class="w-1/2 mx-auto my-8">
-      <cornie-input placeholder="Enter employer's name" class="" />
-      <span class="mt-1 text-grey-blue small text-sm italic"
-        >Enter the primary beneficiary’s employer if you are a covered
-        dependent</span
-      >
-    </div>
+		<div v-if="insuranceprovider === 'employer' && haveInsurance === 'yes'" class="w-1/2 mx-auto my-8">
+			<cornie-input placeholder="Enter employer's name" class="" />
+			<span class="mt-1 text-grey-blue small text-sm italic"
+			>Enter the primary beneficiary’s employer if you are a covered
+				dependent</span
+			>
+		</div>
 
-    <div v-if="insuranceprovider === 'private'" class="text-center">
-      <h4 class="mt-12 text-center">Select your insurance plan</h4>
-      <span class="mt-2 text-grey-blue small-text"
-        >This will help us connect you to a provider that accepts your
-        plan</span
-      >
+		<div v-if="insuranceprovider === 'private' && haveInsurance === 'yes'" class="text-center">
+			<h4 class="mt-12 text-center">Select your insurance plan</h4>
+			<span class="mt-2 text-grey-blue small-text"
+			>This will help us connect you to a provider that accepts your
+				plan</span
+			>
 
-      <form action="" class="w-1/2 mx-auto mt-8">
-        <cornie-select label="Insurance Carrier"> </cornie-select>
-      </form>
-    </div>
+			<form action="" class="w-1/2 mx-auto mt-8">
+				<cornie-select label="Insurance Carrier"> </cornie-select>
+			</form>
+		</div>
 
-    <div class="w-2/3 mx-auto mt-12 flex items-cente justify-center">
-      <c-button
-        class="w-1/2 mr-6"
-        type="button"
-        :primary="true"
-        small
-        @click="$router.push({ path: '/book-appointment/' })"
-      >
-        skip
-      </c-button>
-      <c-button
-        class="w-1/2"
-        type="button"
-        small
-        disabled
-        @click="$router.push({ path: '/book-appointment/' })"
-      >
-        Continue
-      </c-button>
-    </div>
-  </div>
+		<div class="w-2/3 mx-auto mt-12 flex items-cente justify-center">
+			<c-button
+				class="w-1/2 mr-6"
+				type="button"
+				:primary="true"
+				small
+				@click="$router.push({ path: '/book-appointment/' })"
+			>
+				skip
+			</c-button>
+			<c-button
+				class="w-1/2"
+				type="button"
+				small
+				disabled
+				@click="$router.push({ path: '/book-appointment/' })"
+			>
+				Continue
+			</c-button>
+		</div>
+	</div>
 </template>
 
 <script lang="ts">

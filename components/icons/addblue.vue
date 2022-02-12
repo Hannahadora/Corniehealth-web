8092c8db1cf398bbeffc67bf46f024af2d8c3951
Hannah Chiamaka <hannahadora97@gmail.com>
2022-02-12 19:29:59 +0100

    fix lint issues

--- components/icons/addblue.vue
+++ components/icons/addblue.vue
@@ -1,16 +1,16 @@
 <template>
-  <svg
-    width="15"
-    height="14"
-    viewBox="0 0 15 14"
-    fill="none"
-    xmlns="http://www.w3.org/2000/svg"
-  >
-    <path
-      d="M14.3301 8H8.33008V14H6.33008V8H0.330078V6H6.33008V0H8.33008V6H14.3301V8Z"
-      fill="#080056"
-    />
-  </svg>
+	<svg
+		width="15"
+		height="14"
+		viewBox="0 0 15 14"
+		fill="none"
+		xmlns="http://www.w3.org/2000/svg"
+	>
+		<path
+			d="M14.3301 8H8.33008V14H6.33008V8H0.330078V6H6.33008V0H8.33008V6H14.3301V8Z"
+			fill="#080056"
+		/>
+	</svg>
 </template>
 <script>
 export default {

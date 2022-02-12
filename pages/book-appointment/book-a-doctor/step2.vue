db69057e4adc038fd8498fa16c7aec6293593c4d
Hannah Chiamaka <hannahadora97@gmail.com>
2022-02-12 01:47:14 +0100

    update on the insurance page

--- pages/book-appointment/book-a-doctor/step2.vue
+++ pages/book-appointment/book-a-doctor/step2.vue
@@ -1,142 +1,142 @@
 <template>
-  <div class="">
-    <div>
-      <h4 class="mt-12 text-center">Have Insurance?</h4>
+	<div class="">
+		<div>
+			<h4 class="mt-12 text-center">Have Insurance?</h4>
 
-      <form action="" class="flex items-center justify-center mt-4">
-        <div class="flex items-center justify-center">
-          <cornieradio
-            class="mr-9"
-            name="insurance"
-            label="Yes"
-            modelValue="yes"
-            v-model="haveInsurance"
-            value="yes"
-          />
-          <cornieradio
-            name="insurance"
-            label="No"
-            modelValue="no"
-            v-model="haveInsurance"
-            value="no"
-          />
-        </div>
-      </form>
-    </div>
+			<form action="" class="flex items-center justify-center mt-4">
+				<div class="flex items-center justify-center">
+					<cornieradio
+						v-model="haveInsurance"
+						class="mr-9"
+						name="insurance"
+						label="Yes"
+						model-value="yes"
+						value="yes"
+					/>
+					<cornieradio
+						v-model="haveInsurance"
+						name="insurance"
+						label="No"
+						model-value="no"
+						value="no"
+					/>
+				</div>
+			</form>
+		</div>
 
-    <div v-if="haveInsurance === 'yes'" class="mt-8">
-      <h4 class="mt-12 text-center">
-        Is your insurance provided by an employer or private?
-      </h4>
+		<div v-if="haveInsurance === 'yes'" class="mt-8">
+			<h4 class="mt-12 text-center">
+				Is your insurance provided by an employer or private?
+			</h4>
 
-      <form action="" class="flex items-center justify-center mt-4">
-        <div class="flex items-center justify-center">
-          <cornieradio
-            class="mr-9"
-            name="insuranceprovider"
-            label="Employer"
-            modelValue="employer"
-            v-model="insuranceprovider"
-            value="employer"
-          />
-          <cornieradio
-            name="insuranceprovider"
-            label="Private"
-            modelValue="private"
-            v-model="insuranceprovider"
-            value="private"
-          />
-        </div>
-      </form>
-    </div>
+			<form action="" class="flex items-center justify-center mt-4">
+				<div class="flex items-center justify-center">
+					<cornieradio
+						v-model="insuranceprovider"
+						class="mr-9"
+						name="insuranceprovider"
+						label="Employer"
+						model-value="employer"
+						value="employer"
+					/>
+					<cornieradio
+						v-model="insuranceprovider"
+						name="insuranceprovider"
+						label="Private"
+						model-value="private"
+						value="private"
+					/>
+				</div>
+			</form>
+		</div>
 
-    <div
-      v-if="insuranceprovider === 'employer' && haveInsurance === 'yes'"
-      class="w-1/2 mx-auto my-8"
-    >
-      <cornie-input placeholder="Enter employer's name" class="" />
-      <span class="mt-1 text-grey-blue small text-sm italic"
-        >Enter the primary beneficiary’s employer if you are a covered
-        dependent</span
-      >
-    </div>
+		<div
+			v-if="insuranceprovider === 'employer' && haveInsurance === 'yes'"
+			class="w-1/2 mx-auto my-8"
+		>
+			<cornie-input placeholder="Enter employer's name" class="" />
+			<span class="mt-1 text-grey-blue small text-sm italic"
+			>Enter the primary beneficiary’s employer if you are a covered
+				dependent</span
+			>
+		</div>
 
-    <div
-      v-if="insuranceprovider === 'private' && haveInsurance === 'yes'"
-      class="c-w-45 mx-auto text-center"
-    >
-      <h4 class="mt-12 text-center">Select your insurance plan</h4>
-      <span class="mt-2 text-grey-blue small-text"
-        >This will help us connect you to a provider that accepts your
-        plan</span
-      >
+		<div
+			v-if="insuranceprovider === 'private' && haveInsurance === 'yes'"
+			class="c-w-45 mx-auto text-center"
+		>
+			<h4 class="mt-12 text-center">Select your insurance plan</h4>
+			<span class="mt-2 text-grey-blue small-text"
+			>This will help us connect you to a provider that accepts your
+				plan</span
+			>
 
-      <form action="" class="w-full mx-auto mt-8">
-        <div class="mt-8">
-          <label class="text-left block text-xs mb-1 font-bold">Insurance Carrier</label>
-          <cornie-select
-            class="w-full"
-            v-model="insuranceCarrier"
-            placeholder="Select"
-            :readonly="false"
-            :items="['individual', 'family', 'corporate']"
-            required
-            @changed="handleInsuranceCarrier"
-          ></cornie-select>
-        </div>
+			<form action="" class="w-full mx-auto mt-8">
+				<div class="mt-8">
+					<label class="text-left block text-xs mb-1 font-bold">Insurance Carrier</label>
+					<cornie-select
+						v-model="insuranceCarrier"
+						class="w-full"
+						placeholder="Select"
+						:readonly="false"
+						:items="['individual', 'family', 'corporate']"
+						required
+						@changed="handleInsuranceCarrier"
+					></cornie-select>
+				</div>
 
-        <div class="mt-8">
-          <label class="text-left block text-xs mb-1 font-bold">Insurance Plan</label>
-          <cornie-select
-            class="w-full"
-            v-model="insurancePlan"
-            placeholder="Select"
-            :readonly="false"
-            :items="['individual', 'family', 'corporate']"
-            required
-            @changed="handleInsurancePlan"
-          ></cornie-select>
-        </div>
+				<div class="mt-8">
+					<label class="text-left block text-xs mb-1 font-bold">Insurance Plan</label>
+					<cornie-select
+						v-model="insurancePlan"
+						class="w-full"
+						placeholder="Select"
+						:readonly="false"
+						:items="['individual', 'family', 'corporate']"
+						required
+						@changed="handleInsurancePlan"
+					></cornie-select>
+				</div>
 
-        <cornie-input
-          placeholder="Enter policy number"
-          label="Policy Number"
-          class="mt-8 w-full"
-          v-model="policyNumber"
-        />
+				<cornie-input
+					v-model="policyNumber"
+					placeholder="Enter policy number"
+					label="Policy Number"
+					class="mt-8 w-full"
+				/>
 
-        <cornie-checkbox
-          class="mt-8"
-          name="billAgreement"
-          v-model="billInsurance"
-          label="Bill my insurance for this service."
-		  @change="handleAgree"
-        />
-      </form>
-    </div>
+				<cornie-checkbox
+					v-model="billInsurance"
+					class="mt-8"
+					name="billAgreement"
+					label="Bill my insurance for this service."
+					@change="handleAgree"
+				/>
+			</form>
+		</div>
 
-    <div class="w-2/5 mx-auto mt-12 flex items-cente justify-center">
-      <c-button
-        class="w-1/2 mr-6"
-        n
-        type="button"
-        :primary="true"
-        small
-        @click="$router.push({ path: '/book-appointment/' })"
-      >
-        skip
-      </c-button>
-      <c-button
-        class="w-1/2"
-        type="button"
-        secondary
-        small
-        @click="$router.push({ path: '/book-appointment/confirm-payment' })"
-      >
-        Continue
-      </c-button>
-    </div>
-  </div>
+		<div class="w-2/5 mx-auto mt-12 flex items-cente justify-center">
+			<c-button
+				class="w-1/2 mr-6"
+				n
+				type="button"
+				:primary="true"
+				small
+				@click="$router.push({ path: '/book-appointment/' })"
+			>
+				skip
+			</c-button>
+			<c-button
+				class="w-1/2"
+				type="button"
+				secondary
+				small
+				@click="$router.push({ path: '/book-appointment/confirm-payment' })"
+			>
+				Continue
+			</c-button>
+		</div>
+	</div>
 </template>
 
 <script lang="ts">
@@ -167,9 +167,11 @@ export default class BookDoctorPage extends Vue {
   handleInsuranceCarrier(val: any) {
     this.insuranceCarrier = val
   }
+
   handleInsurancePlan(val: any) {
     this.insurancePlan = val
   }
+
   handleAgree(val: any) {
     this.billInsurance = val
   }

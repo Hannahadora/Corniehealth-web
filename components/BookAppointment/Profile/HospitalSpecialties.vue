9fb50cd384598e105b8e740acd0188a1c18edcb0
Hannah Chiamaka <hannahadora97@gmail.com>
2022-02-15 12:52:19 +0100

    upate on hospital profile and serch result

--- components/BookAppointment/Profile/HospitalSpecialties.vue
+++ components/BookAppointment/Profile/HospitalSpecialties.vue
@@ -1,13 +1,20 @@
 <template>
-  <div class="flex items-center flex-wrap">
-    <div class="specialty-box mr-8">
-        <div class="flex items-center justify-between">
-            <p class="text-grey-blue">All</p>
-            <img src="/images/book-appointment/icon-doctor-grey" alt="">
-        </div>
-        <p></p>
-    </div>
-  </div>
+	<div class="flex items-center justify-between flex-wrap">
+		<div class="specialty-box xl:mb-0 mb-8">
+			<div class="flex items-center justify-between">
+				<p class="text-grey-blue">All</p>
+				<img src="/images/book-appointment/icon-doctor-grey.png" alt="">
+			</div>
+			<p class="mt-2 font-bold">18</p>
+		</div>
+		<div v-for="(specialty, index) in specialties" :key="index" class="specialty-box xl:mb-0 mb-8">
+			<div class="flex items-center justify-between">
+				<p class="text-grey-blue">{{ specialty.name }}</p>
+				<img src="/images/book-appointment/icon-doctor-grey.png" alt="">
+			</div>
+			<p class="mt-2 font-bold">{{ specialty.total }}</p>
+		</div>
+	</div>
 </template>
 
 <script lang="ts">
@@ -15,7 +22,23 @@ import { Component, Vue } from "nuxt-property-decorator"
 @Component({
   components: {},
 })
-export default class HospitalSpecialties extends Vue {}
+export default class HospitalSpecialties extends Vue {
+    specialties: Array<any> = []
+
+
+    getSpecialties() {
+        return this.specialties = [
+            { name: 'General Practice', total: '4' },
+            { name: 'OBGYN', total: '5' },
+            { name: 'Pediatrics', total: '6' },
+            { name: 'Emergency Medicine', total: '3' }
+        ]
+    }
+
+    created() {
+       this.getSpecialties()
+    }
+}
 </script>
 
 <style scoped>

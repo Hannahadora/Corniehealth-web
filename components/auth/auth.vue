4f583704e86331bfb3a7a0a9327d2b6d6d43b384
sherifowolabi <sherif.owolabi@scelloo.com>
2022-02-17 16:48:09 +0100

    Made updates to early access UI

--- components/auth/auth.vue
+++ components/auth/auth.vue
@@ -1,34 +1,34 @@
 <template>
-  <div
-    class="px-5 w-full h-screen bg-primary lg:grid grid-cols-12 overflow-y-auto overflow-x-hidden py-5 md:py-0"
-  >
-    <div class="lg:col-span-6 lg:h-full lg:relative lg:pr-20">
-      <nuxt-link to="/" class="lg:absolute top-3 left-0 cursor-pointer z-40">
-        <logo-icon class="fill-current w-14 h-14 lg:w-20 lg:h-20" />
-      </nuxt-link>
+	<div
+		class="px-5 w-full h-screen bg-primary lg:grid grid-cols-12 overflow-y-auto overflow-x-hidden py-5 md:py-0"
+	>
+		<div class="lg:col-span-6 lg:h-full lg:relative lg:pr-20">
+			<nuxt-link to="/" class="lg:absolute top-3 left-0 cursor-pointer z-40">
+				<logo-icon class="fill-current w-14 h-14 lg:w-20 lg:h-20" />
+			</nuxt-link>
 
-      <div
-        class="relative bottom-0 overflow-hidden mx-auto w-full h-full md:w-3/4"
-      >
-        <img
-          v-if="!$slots.image"
-          src="@/static/images/smiling_lady.png"
-          class="w-full h-full"
-        />
-        <slot name="image"></slot>
-        <div
-          v-if="$slots.banner"
-          class="absolute h-auto w-full bg-primary left-0 right-0 bottom-0 z-10 mr-10 p-3 flex flex-col justify-center items-center text-white"
-        >
-          <slot name="banner"></slot>
-        </div>
-      </div>
-    </div>
+			<div
+				class="relative bottom-0 overflow-hidden mx-auto w-full h-full md:w-3/4"
+			>
+				<img
+					v-if="!$slots.image"
+					src="@/static/images/smiling_lady.png"
+					class="w-full h-full"
+				/>
+				<slot name="image"></slot>
+				<div
+					v-if="$slots.banner"
+					class="absolute h-auto w-full bg-primary left-0 right-0 bottom-0 z-10 mr-10 p-3 flex flex-col justify-center items-center text-white"
+				>
+					<slot name="banner"></slot>
+				</div>
+			</div>
+		</div>
 
-    <div class="lg:col-span-6">
-      <slot />
-    </div>
-  </div>
+		<div class="lg:col-span-6">
+			<slot />
+		</div>
+	</div>
 </template>
 
 <script>

c8b315d59b627075d773fd6a2ad272eea57873e9
sherifowolabi <sherif.owolabi@scelloo.com>
2022-02-21 11:45:13 +0100

    Change mobile to Phone number in early access form.

--- pages/signup/Payer.vue
+++ pages/signup/Payer.vue
@@ -1,122 +1,122 @@
 <template>
-	<div>
-		<cornie-dialog
-			v-if="showDiag"
-			:firstname="form.firstName"
-			@close-diag="hideDialog"
-		/>
-		<auth>
-			<template #default>
-				<div class="w-full h-full flex justify-center items-center">
-					<div
-						class="w-full h-auto bg-white rounded-md shadow-md p-5 overflow-hidden"
-					>
-						<h2 class="text-sm text-primary">Early Access Registration</h2>
-						<ValidationObserver v-slot="{}">
-							<form class="h-auto mt-10" @submit.prevent="submit">
-								<div class="md:grid grid-cols-12 gap-3">
-									<div class="col-span-6 mb-3">
-										<cornie-input
-											v-model="form.firstName"
-											label="First Name"
-											placeholder="--Enter--"
-											required
-										></cornie-input>
-									</div>
-									<div class="col-span-6 mb-3">
-										<cornie-input
-											v-model="form.lastName"
-											label="Last Name"
-											placeholder="--Enter--"
-											required
-										></cornie-input>
-									</div>
-									<div class="col-span-6 mb-3">
-										<cornie-input
-											v-model="form.phoneNumber.number"
-											label="Mobile"
-											type="tel"
-											placeholder="--Enter--"
-											required
-										></cornie-input>
-									</div>
-									<div class="col-span-6 mb-3">
-										<ValidationProvider
-											v-slot="{ errors }"
-											name="email"
-											rules="email"
-										>
-											<cornie-input
-												v-model="form.email"
-												label="Email Addrees"
-												placeholder="--Enter--"
-												type="email"
-												required
-											></cornie-input>
-											<span class="text-red-500 text-xs">{{ errors[0] }}</span>
-										</ValidationProvider>
-									</div>
-									<div class="col-span-12 mb-3">
-										<cornie-input
-											v-model="form.organizationName"
-											label="Organization Name"
-											placeholder="--Enter--"
-											required
-										></cornie-input>
-									</div>
-								</div>
-								<div class="w-full flex items-center">
-									<cornie-checkbox
-										v-model="agree"
-										class="mr-1"
-										@change="handleAgree"
-									/>
-									<div class="text-sm">
-										I agree to CorieHealth's
-										<a href="#" class="text-red-400">Terms of service</a> and
-										<a href="#" class="text-red-400">Privacy policy</a>
-									</div>
-								</div>
-								<div class="w-full mt-5">
-									<button
-										class="rounded-md w-full text-center font-bold py-2"
-										:class="[
-											!formCompleted
-												? 'bg-gray-300 text-gray-400'
-												: 'bg-red-500 text-white hover:bg-red-400',
-										]"
-									>
-										Submit
-									</button>
-								</div>
-							</form>
-						</ValidationObserver>
-						<div class="my-6 w-full text-center">
-							Have an account?
-							<span
-								class="text-red-400 hover:text-red-300 cursor-pointer"
-								@click="handleSignin"
-							>Sign in</span
-							>
-						</div>
-						<div class="w-full text-center">
-							<a href="#">Terms of use</a> | <a href="#">Privacy policy</a> |
-							<a href="#">Help</a> | 2021 Cornie Health Ltd.
-						</div>
-					</div>
-				</div>
-			</template>
-			<template #banner>
-				<div class="flex justify-center items-center">
-					<heart-pulse class="mr-3" />
-					<div class="text-xs">Connectd. Patient-Centric. Supportive</div>
-				</div>
-				<div class="text-center text-2xl font-bold pb-5">
-					Connected, Simple Practice Management Software.
-				</div>
-			</template>
-		</auth>
-	</div>
+  <div>
+    <cornie-dialog
+      v-if="showDiag"
+      :firstname="form.firstName"
+      @close-diag="hideDialog"
+    />
+    <auth>
+      <template #default>
+        <div class="w-full h-full flex justify-center items-center">
+          <div
+            class="w-full h-auto bg-white rounded-md shadow-md p-5 overflow-hidden"
+          >
+            <h2 class="text-sm text-primary">Early Access Registration</h2>
+            <ValidationObserver v-slot="{}">
+              <form class="h-auto mt-10" @submit.prevent="submit">
+                <div class="md:grid grid-cols-12 gap-3">
+                  <div class="col-span-6 mb-3">
+                    <cornie-input
+                      v-model="form.firstName"
+                      label="First Name"
+                      placeholder="--Enter--"
+                      required
+                    ></cornie-input>
+                  </div>
+                  <div class="col-span-6 mb-3">
+                    <cornie-input
+                      v-model="form.lastName"
+                      label="Last Name"
+                      placeholder="--Enter--"
+                      required
+                    ></cornie-input>
+                  </div>
+                  <div class="col-span-6 mb-3">
+                    <cornie-input
+                      v-model="form.phoneNumber.number"
+                      label="Phone Number"
+                      type="tel"
+                      placeholder="--Enter--"
+                      required
+                    ></cornie-input>
+                  </div>
+                  <div class="col-span-6 mb-3">
+                    <ValidationProvider
+                      v-slot="{ errors }"
+                      name="email"
+                      rules="email"
+                    >
+                      <cornie-input
+                        v-model="form.email"
+                        label="Email Addrees"
+                        placeholder="--Enter--"
+                        type="email"
+                        required
+                      ></cornie-input>
+                      <span class="text-red-500 text-xs">{{ errors[0] }}</span>
+                    </ValidationProvider>
+                  </div>
+                  <div class="col-span-12 mb-3">
+                    <cornie-input
+                      v-model="form.organizationName"
+                      label="Organization Name"
+                      placeholder="--Enter--"
+                      required
+                    ></cornie-input>
+                  </div>
+                </div>
+                <div class="w-full flex items-center">
+                  <cornie-checkbox
+                    v-model="agree"
+                    class="mr-1"
+                    @change="handleAgree"
+                  />
+                  <div class="text-sm">
+                    I agree to CorieHealth's
+                    <a href="#" class="text-red-400">Terms of service</a> and
+                    <a href="#" class="text-red-400">Privacy policy</a>
+                  </div>
+                </div>
+                <div class="w-full mt-5">
+                  <button
+                    class="rounded-md w-full text-center font-bold py-2"
+                    :class="[
+                      !formCompleted
+                        ? 'bg-gray-300 text-gray-400'
+                        : 'bg-red-500 text-white hover:bg-red-400',
+                    ]"
+                  >
+                    Submit
+                  </button>
+                </div>
+              </form>
+            </ValidationObserver>
+            <div class="my-6 w-full text-center">
+              Have an account?
+              <span
+                class="text-red-400 hover:text-red-300 cursor-pointer"
+                @click="handleSignin"
+                >Sign in</span
+              >
+            </div>
+            <div class="w-full text-center">
+              <a href="#">Terms of use</a> | <a href="#">Privacy policy</a> |
+              <a href="#">Help</a> | 2021 Cornie Health Ltd.
+            </div>
+          </div>
+        </div>
+      </template>
+      <template #banner>
+        <div class="flex justify-center items-center">
+          <heart-pulse class="mr-3" />
+          <div class="text-xs">Connectd. Patient-Centric. Supportive</div>
+        </div>
+        <div class="text-center text-2xl font-bold pb-5">
+          Connected, Simple Practice Management Software.
+        </div>
+      </template>
+    </auth>
+  </div>
 </template>
 
 <script>

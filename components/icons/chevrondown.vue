9c05fada758f08e0d69e2352ae7d330e212f1ab5
Hannah Chiamaka <hannahadora97@gmail.com>
2022-04-04 00:05:48 +0100

    update on booking-site

---
+++ components/icons/chevrondown.vue
@@ -0,0 +1,23 @@
+<template>
+	<svg
+		xmlns="http://www.w3.org/2000/svg"
+		width="12"
+		height="8"
+		viewBox="0 0 12 8"
+		fill="none"
+		stroke="white"
+	>
+		<path
+			d="M11 1.5L6 6.5L1 1.5"
+			stroke-width="2"
+			stroke-linecap="round"
+			stroke-linejoin="round"
+		/>
+	</svg>
+</template>
+<script lang="ts">
+import { Component, Vue } from "nuxt-property-decorator"
+
+@Component({})
+export default class ChevronDownIcon extends Vue {}
+</script>
--- components/multiselectsearch.vue
+++ components/multiselectsearch.vue
@@ -1,5 +1,5 @@
 <template>
-	<div class="">
+	<div class="relative">
 		<div v-if="label" class="mb-2">
 			<label>{{ label }}</label>
 		</div>

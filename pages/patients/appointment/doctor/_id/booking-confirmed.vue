52f0a9300650d3d41294cf48c9736c32b5081168
Hannah Chiamaka <hannahadora97@gmail.com>
2022-08-03 16:15:16 +0100

    update

--- pages/patients/appointment/doctor/_id/booking-confirmed.vue
+++ pages/patients/appointment/doctor/_id/booking-confirmed.vue
@@ -1,96 +1,126 @@
 <template>
-	<div class="c-wrapper">
-		<div class="xl:w-2/3 w-full mx-auto xl:mt-24 mt-14">
-			<h2 class="xl:text-center text-left c-indigo mb-12">Confirm your appointment</h2>
+  <div class="c-wrapper">
+    <back-btn />
+    <div class="xl:w-2/3 w-full mx-auto xl:mt-4 mt-2">
+      <h2 class="xl:text-center text-left c-indigo mb-12">
+        Confirm your appointment
+      </h2>
 
-			<table class="border w-full">
-				<tr>
-					<td>Appointment With</td>
-					<td>Dr. Mike Obi</td>
-				</tr>
-				<tr>
-					<td>Date & Time</td>
-					<td>29, November 2021 | 21:00</td>
-				</tr>
-				<tr>
-					<td>Date & Time</td>
-					<td>29, November 2021 | 21:00</td>
-				</tr>
-				<tr>
-					<td>Consultation Fee</td>
-					<td>₦ 20,000.00</td>
-				</tr>
-				<tr>
-					<td>Specialty</td>
-					<td>Dentistry</td>
-				</tr>
-				<tr>
-					<td>Location</td>
-					<td>Reddington Hospital | 234 Admiralty Way Lekki, Lagos, Nigeria</td>
-				</tr>
-				<tr>
-					<td>Contact Info</td>
-					<td>+234 803 767 8909 | Mike.obi@reddington.ng</td>
-				</tr>
-				<tr>
-					<td>Reason for Appointmment</td>
-					<td>Consultation</td>
-				</tr>
-			</table>
+      <table class="border w-full">
+        <tr>
+          <td>Appointment With</td>
+          <td>{{ selectedPractitioner.name }}</td>
+        </tr>
+        <tr>
+          <td>Date & Time</td>
+          <td>{{ getSelectedDate }} | {{ getSelectedTime }}</td>
+        </tr>
+        <tr>
+          <td>Consultation Fee</td>
+          <td>₦ {{ selectedPractitioner.ConsultationFeePerHour || 0 }}</td>
+        </tr>
+        <tr>
+          <td>Specialty</td>
+          <td>{{ selectedPractitioner.designation }}</td>
+        </tr>
+        <tr>
+          <td>Location</td>
+          <td>{{ selectedPractitioner.address }}</td>
+        </tr>
+        <tr>
+          <td>Contact Info</td>
+          <td>
+            {{ selectedPractitioner.phone }} | {{ selectedPractitioner.email }}
+          </td>
+        </tr>
+        <tr>
+          <td>Reason for Appointmment</td>
+          <td>Consultation</td>
+        </tr>
+      </table>
 
-			<p class="my-8">
-				You may wish to share additional information about your condition and/or
-				the reason for this appointment. Should you have any emergency, kindly
-				contact your practitioner by phone.
-			</p>
+      <p class="my-8">
+        You may wish to share additional information about your condition and/or
+        the reason for this appointment. Should you have any emergency, kindly
+        contact your practitioner by phone.
+      </p>
 
-			<div class="flex flex-col w-full mb-20">
-				<label class="text-left block text-xs mb-1 font-bold" for=""
-				>Note</label
-				>
-				<textarea
-					id=""
-					placeholder="Write note for practitioner here"
-					class="border p-2"
-					name=""
-					cols="20"
-					rows="10"
-				></textarea>
-				<span class="text-right text-xs italic font-semibold">0/255</span>
-			</div>
+      <div class="flex flex-col w-full mb-20">
+        <label class="text-left block text-xs mb-1 font-bold" for=""
+          >Note</label
+        >
+        <textarea
+          id=""
+          placeholder="Write note for practitioner here"
+          class="border p-2"
+          name=""
+          cols="20"
+          rows="10"
+		  maxlength="255"
+        ></textarea>
+        <span class="text-right text-xs italic font-semibold">0/255</span>
+      </div>
 
-			<div class="w-full mx-auto mt-12 mb-72 flex items-center justify-center">
-				<c-button
-					class="w-1/2 mr-6"
-					type="button"
-					:primary="true"
-					small
-					@click="$router.push({ path: '/book-appointment/' })"
-				>
-					Cancel
-				</c-button>
-				<c-button
-					class="w-1/2"
-					type="button"
-					secondary
-					small
-					@click="$router.push({ path: '/book-appointment/confirm-payment' })"
-				>
-					Confirm
-				</c-button>
-			</div>
-		</div>
-	</div>
+      <div class="w-full mx-auto mt-12 mb-72 flex items-center justify-center">
+        <c-button
+          class="w-1/2 mr-6"
+          type="button"
+          :primary="true"
+          small
+          @click="$router.go(-1)"
+        >
+          Cancel
+        </c-button>
+        <c-button
+          class="w-1/2"
+          type="button"
+          secondary
+          small
+          @click="confirmPayment"
+        >
+          Confirm
+        </c-button>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from "nuxt-property-decorator"
-// import CButton from "~/components/CButton.vue"
+import CButton from "~/components/CButton.vue"
+import BackBtn from "~/components/BackBtn.vue"
+import { namespace } from "vuex-class"
+
+const practitioners = namespace("practitioners")
+const appointment = namespace("appointment")
 @Component({
-  //   components: { CButton },
+    components: { CButton, BackBtn },
   layout: "appointment",
 })
-export default class BookingConfirmed extends Vue {}
+export default class BookingConfirmed extends Vue {  
+	@practitioners.Getter
+  selectedPractitioner!: any
+  
+  @appointment.Getter
+    getSelectedTime!: ""
+
+  @appointment.Getter
+    getSelectedDate!: ""
+
+  async created() {
+    if (this.$route.query.practitioner) {
+      await this.$store.dispatch(
+        "practitioners/getAPractitionerProfile",
+        this.$route.params.id
+      )
+    }
+  }
+
+	confirmPayment() {
+		alert('Booking-confirmed')
+		this.$router.push('/')
+	}
+}
 </script>
 
 <style scoped>
@@ -99,7 +129,7 @@ tr {
   line-break: normal;
 }
 td {
-	font-size: 16px;
+  font-size: 16px;
   padding: 16px;
 }
 tr:nth-child(even) {
@@ -107,11 +137,11 @@ tr:nth-child(even) {
 }
 
 @media screen and (max-width: 768px) {
-	tr {
-		line-break: auto;
-	}
-	td {
-		font-size: 14px;
-	}
+  tr {
+    line-break: auto;
+  }
+  td {
+    font-size: 14px;
+  }
 }
 </style>
